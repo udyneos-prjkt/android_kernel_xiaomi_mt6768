541259f57a3afe9e4d7df148ad3e1d0b9b676977
Kalesh Singh <kaleshsingh@google.com>
2022-04-22 08:47:55 -0700

    ANDROID: Make MGLRU aware of speculative faults
    Bug: 228525049
    Bug: 227651406
    Change-Id: Ib3d45f74c5c23cbb1af7aaf95c90826baf406c7a
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4514,6 +4514,9 @@ static int __handle_mm_fault(struct vm_area_struct *vma, unsigned long address,
 	return handle_pte_fault(&vmf);
 }
 
+static void lru_gen_enter_fault(struct vm_area_struct *vma);
+static void lru_gen_exit_fault(void);
+
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
 
 #ifndef spf_pxd_flunked
@@ -4748,10 +4751,13 @@ int __handle_speculative_fault(struct mm_struct *mm, unsigned long address,
 		goto out_put;
 	}
 
-	mem_cgroup_oom_enable();
-	ret = handle_pte_fault(&vmf);
 	/* NOTE: vmf.pte should be unmapped after handle_pte_fault */
 	mem_cgroup_oom_disable();
+	mem_cgroup_enter_user_fault();
+	lru_gen_enter_fault(vmf.vma);
+	ret = handle_pte_fault(&vmf);
+	lru_gen_exit_fault();
+	mem_cgroup_exit_user_fault();
 
 	put_vma(vma);
 
